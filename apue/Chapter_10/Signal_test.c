7140232d29df350d60cfad9d9399ccd19203ac2d
weitao <weitaoee@gmail.com>
2017-09-12 19:41:47 +0800

    add

---
+++ apue/Chapter_10/Signal_test.c
@@ -0,0 +1,37 @@
+#include "apue.h"
+
+static void SigUsr(int);
+
+int main()
+{
+	if (signal(SIGUSR1, SigUsr) == SIG_ERR)
+	{
+		err_sys("Can't catch SIGUSR1");
+	}
+	if (signal(SIGUSR2, SigUsr) == SIG_ERR)
+	{
+		err_sys("Can't catch SIGUSR2");
+	}
+
+	while(1)
+	{
+		pause();
+	}
+	exit(0);
+}
+
+static void SigUsr(int p_signo)
+{
+	if (p_signo == SIGUSR1)
+	{
+		fprintf(stderr, "received SIGUSR1\n");
+	}
+	else if (p_signo == SIGUSR2)
+	{
+		fprintf(stderr, "received SIGUSR2\n");
+	}
+	else
+	{
+		err_dump("received signal %d\n", p_signo);
+	}
+}
