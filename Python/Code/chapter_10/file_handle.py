121aed5d07f68653c07f40d9df8c2165a450e3ae
weee <weitaoee@gmail.com>
2017-10-17 22:52:29 +0800

    file data deal

--- Python/Code/chapter_10/file_handle.py
+++ Python/Code/chapter_10/file_handle.py
@@ -5,10 +5,19 @@ usage_str = "Please input userdata filename: "
 file_name = input(usage_str)
 file_name += ".txt"
 
+usage_str = "Please input savedata filename: "
+save_file = input(usage_str)
+save_file += ".txt"
 
-def file_handle(file_name):
+def read_file(file_name):
 	with open(file_name) as file_obj:
-		for line in file_obj:
-			print(line.rstrip())
+		file_lines = file_obj.readlines()
+	return file_lines
 
-file_handle(file_name)
+def write_file(w_file, data):
+	with open(w_file, "w") as file_obj:
+		for line in data:
+			file_obj.write(line)
+
+file_data = read_file(file_name)
+write_file(save_file, file_data)
