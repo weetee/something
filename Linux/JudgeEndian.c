071d676ee7dfd93d9746c80294a7e07881943547
weitao <weitaoee@gmail.com>
2017-12-21 15:57:13 +0800

    c

--- Linux/JudgeEndian.c
+++ Linux/JudgeEndian.c
@@ -8,9 +8,16 @@ void ByteOrder()
 		char m_bytes[sizeof(short)];
 	} test;
 	test.m_value = 0x0305;
+	if (test.m_bytes[0] == 3 && test.m_bytes[1] == 5)
+		printf("big endian~!\n");
+	else if (test.m_bytes[0] == 5 && test.m_bytes[1] == 3)
+		printf("little endian~!\n");
+	else
+		printf("unknow endian!!\n");
 }
 
 int main()
 {
-
+	ByteOrder();
+	return 0;
 }
