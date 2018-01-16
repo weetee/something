67f1d2d4d5e0048d2e856d885f9474c92c4f3067
weitao <choosewee@gmail.com>
2018-01-16 23:10:02 +0800

    2

---
+++ C++/C++Primer/Chapter_2/book_2_3_3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	int i = 10;
+	int *p_i = &i;
+	int *&rp_i = p_i;
+
+	*p_i = 20;
+	*rp_i = 5;
+
+	std::cout << "p_i = " << *p_i << std::endl;
+	std::cout << "rp_i = " << *rp_i << std::endl;
+
+	int **pp_i = &p_i;
+	int **&rp_i2 = pp_i;
+	std::cout << "rp_i2 = " << **rp_i2 << std::endl;
+
+	return 0;
+}
