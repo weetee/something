bf23bedaccd1dac8c0a75da5577a94fc4fd500a7
weee <weitaoee@gmail.com>
2018-01-21 14:20:41 +0800

    7

--- C++/C++Primer/Chapter_7/7_27.cpp
+++ C++/C++Primer/Chapter_7/7_27.cpp
@@ -4,9 +4,9 @@ int main()
 {
 	Screen my_screen(5,5,'X');
 	my_screen.MoveCursor(4, 0).SetCharacter('#').Display(std::cout);
-	cout << "\n";
+	std::cout << "\n";
 	my_screen.Display(std::cout);
-	cout << "\n";
+	std::cout << "\n";
 
 	return 0;
 }
