96d3411850e518c8dc8558bf7be3a87cbe676900
weee <weitaoee@gmail.com>
2018-01-25 18:23:39 +0800

    8

---
+++ C++/C++Primer/Chapter_8/8_5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        cout << "Need read file : " << endl;
+        return -1;
+    }
+    return 0;
+}
