d33e4aa0881d78d7956d26fb8ef4ee348da126b7
weee <weitaoee@gmail.com>
2018-01-26 12:28:37 +0800

    8

--- C++/C++Primer/Chapter_8/8_5.cpp
+++ C++/C++Primer/Chapter_8/8_5.cpp
@@ -12,5 +12,19 @@ int main(int argc, char **argv)
         cout << "Need read file : " << endl;
         return -1;
     }
+    ifstream in(argv[1]);
+    vector<string> str_vec;
+    if (in)
+    {
+        //string line;
+        //while (getline(in, line)) 
+            //str_vec.push_back(line);
+        string word;
+        while (in >> word)
+            str_vec.push_back(word);
+    }
+    auto beg_itr = str_vec.cbegin();
+    while (beg_itr != str_vec.cend())
+        cout << *beg_itr++ << endl;
     return 0;
 }
