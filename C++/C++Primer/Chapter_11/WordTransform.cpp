732e5a1617425d3527d1dc5e78dcd0dc3dc3fc5d
weee <weitaoee@gmail.com>
2018-01-31 12:32:02 +0800

    11

--- C++/C++Primer/Chapter_11/WordTransform.cpp
+++ C++/C++Primer/Chapter_11/WordTransform.cpp
@@ -6,4 +6,13 @@
 
 using namespace std;
 
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        cout << "Need two files,first is rule file; second is need transform file" << endl;
+        return -1;
+    }
 
+    return 0;
+}
